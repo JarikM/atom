205c7e46d8e9e525b35508260543a8b38698a8db
Iaroslav Maksiuk <Jarek@192.168.10.17>
2018-09-16 16:56:45 +0300

    failed unit tests were fixed in /homeworks/HW1/src/test/java/ru/atom/UtilTest.java

--- homeworks/HW1/src/main/java/ru.atom/Util.java
+++ homeworks/HW1/src/main/java/ru.atom/Util.java
@@ -1,5 +1,11 @@
 package ru.atom;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * In this assignment you need to implement the following util methods.
  * Note:
@@ -16,7 +22,19 @@ public class Util {
      * @return the largest of values.
      */
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        // converting int[] to Integer[] --- VARIANT 1
+        Integer[] val = new Integer[values.length];
+        for (int i = 0; i < values.length; i++) {
+            val[i] = Integer.valueOf(values[i]);
+        }
+
+        int max = Collections.max(Arrays.asList(val));
+        return max;
+
+        // converting int[] to Integer[] --- VARIANT 2
+        //Integer[] val = Arrays.stream(values).boxed().toArray(Integer[]::new);
+        // converting int[] to Integer[] --- VARIANT 3
+        //List<Integer> val = IntStream.of(values).boxed().collect(Collectors.toList());
     }
 
     /**
@@ -26,8 +44,11 @@ public class Util {
      * @return the sum of all values.
      */
     public static long sum(int[] values) {
-        throw new UnsupportedOperationException();
+        long sum = 0;
+        for (long i : values) {
+            sum += i;
+        }
+        return sum;
     }
-
 
 }
